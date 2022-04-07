a4aa4ad34fa931687638944f386dc42b55a8a629
Andre Ruiz <andrelucasruizalmeida@gmail.com>
2022-04-06 21:00:43 -0300

    feat: configuração inicial backend com spring

---
+++ backend/src/test/java/br/edu/utfpr/pw/creche/CrecheApplicationTests.java
@@ -0,0 +1,13 @@
+package br.edu.utfpr.pw.creche;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class CrecheApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
