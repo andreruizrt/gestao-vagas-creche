9b8b48ba0c3060fe840b444bde2b33b23ae78e00
Andre Ruiz <andrelucasruizalmeida@gmail.com>
2022-04-09 13:05:02 -0300

    feat: página inicial do dashboard do site

---
+++ frontend/src/pages/dashboard/index.tsx
@@ -0,0 +1,15 @@
+import { Box, Heading } from '@chakra-ui/react'
+
+import Navbar from '../../components/Nav/Navbar'
+
+const Index = () => {
+  return (
+    <Box>
+      <Navbar />
+      <Heading>Dashboard</Heading>
+    </Box>
+  )
+}
+
+
+export default Index;
