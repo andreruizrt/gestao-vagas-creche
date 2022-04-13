637d53f6ff419862522009c486619fa9b6172e08
Andre Ruiz <andrelucasruizalmeida@gmail.com>
2022-04-12 23:41:44 -0300

    feat: arquivo de insert de usuarios para teste em banco

---
+++ backend/src/main/resources/mock_sql/INSERTS_USERS_MOCK.sql
@@ -0,0 +1,8 @@
+insert into usuario (id, username, password, email) values (1, 'cfawdry0', 'nkcf6LdS9v', 'tlehrian0@addthis.com');
+insert into usuario (id, username, password, email) values (2, 'gskurray1', 's86u6Yv7', 'amitkin1@nsw.gov.au');
+insert into usuario (id, username, password, email) values (3, 'lmacleese2', 'go7nDA', 'mvaller2@apple.com');
+insert into usuario (id, username, password, email) values (4, 'kabrahamoff3', 'aydapNyN', 'pdressel3@godaddy.com');
+insert into usuario (id, username, password, email) values (5, 'rgrelik4', 'Xp856Fkbxo4r', 'egodin4@netscape.com');
+insert into usuario (id, username, password, email) values (6, 'lhaile5', 'nD2hf1Ptxdg', 'dokelly5@princeton.edu');
+insert into usuario (id, username, password, email) values (7, 'acurds6', 'tQHL7KoX', 'ewagg6@latimes.com');
+insert into usuario (id, username, password, email) values (8, 'dmcalinion7', 'oGnNdgWwuS', 'cbatram7@bbc.co.uk');
