6946a5527818399c831bff7fa9365750899eb4f7
Andre Ruiz <andrelucasruizalmeida@gmail.com>
2022-04-12 23:42:42 -0300

    refactor: remove importação não utilizada

--- backend/src/main/java/br/edu/utfpr/pw/creche/controller/UsuarioController.java
+++ backend/src/main/java/br/edu/utfpr/pw/creche/controller/UsuarioController.java
@@ -5,7 +5,6 @@ import java.util.List;
 import java.util.Optional;
 
 import org.springframework.beans.factory.annotation.Autowired;
-import org.springframework.cache.annotation.CacheConfig;
 import org.springframework.http.HttpStatus;
 import org.springframework.http.ResponseEntity;
 import org.springframework.web.bind.annotation.CrossOrigin;
@@ -16,7 +15,6 @@ import org.springframework.web.bind.annotation.PostMapping;
 import org.springframework.web.bind.annotation.PutMapping;
 import org.springframework.web.bind.annotation.RequestBody;
 import org.springframework.web.bind.annotation.RequestMapping;
-import org.springframework.web.bind.annotation.RequestParam;
 import org.springframework.web.bind.annotation.RestController;
 
 import br.edu.utfpr.pw.creche.model.Usuario;
