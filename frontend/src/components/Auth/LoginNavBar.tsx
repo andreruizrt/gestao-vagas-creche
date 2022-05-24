b6a621519ea0d3b2696b3fee647e82d2d86d2458
Andre Almeida <andrelucasruizalmeida@gmail.com>
2022-05-23 23:09:01 -0300

    feat: verifica user para apresentar navbar personalizado

--- frontend/src/components/Auth/LoginNavBar.tsx
+++ frontend/src/components/Auth/LoginNavBar.tsx
@@ -1,20 +1,24 @@
-import { useState, useEffect } from 'react';
+import { useState, useEffect, useContext } from 'react';
+import { AuthContext } from '../../contexts/AuthContext';
 
 import LoggedUserMenuOptions from './LoggedUserMenuOptions';
 import LoginCadastrarButtons from './LoginCadatrarButtons';
 
 export default function LoginNavBar() {
-  const [isLogged, setIsLogged] = useState(false);
-  
-  return (<>
-    {
-      isLogged ? (
-        <LoginCadastrarButtons />
-      ) : (
-        <LoggedUserMenuOptions />
-      )
-    }
-  </>
+  const { user } = useContext(AuthContext);
+
+  const isLogged = !!user;
+
+  return (
+    <>
+      {
+        isLogged ? (
+          <LoggedUserMenuOptions />
+        ) : (
+          <LoginCadastrarButtons />
+        )
+      }
+    </>
   );
 
 }
