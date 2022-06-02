d468c50f1565d548fd460e23f868715eb37419fb
Andre Almeida <andrelucasruizalmeida@gmail.com>
2022-06-02 00:12:39 -0300

    refactor: altera para pegar resultado direto do response - deverá se corrigido

--- frontend/src/contexts/AuthContext.tsx
+++ frontend/src/contexts/AuthContext.tsx
@@ -36,8 +36,8 @@ export function AuthProvider({ children }) {
         const { 'nextauth.token': token } = parseCookies();
 
         if (token) {
-            recoverUserInformation().then(response => {
-                setUser(response.user);
+            recoverUserInformation(token).then(response => {
+                setUser(response);
             });
         }
     }, []);
